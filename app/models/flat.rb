2e79d1eadc8340a08ebc25db09201b6430ad7985
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-23 01:07:13 +0100

    adding few things :)

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,6 +3,9 @@ class Flat < ApplicationRecord
   has_many :bookings
   has_attachments :photos, maximum: 3
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   # validates :name, presence: true, uniqueness: true
   # validates :address, presence: true, uniqueness: true
   # validates :price_night, presence: true
