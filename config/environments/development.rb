187c671b09ab2ef6b34fd033891c7699348c46ee
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-24 11:59:52 +0100

    mailer done, mail send from my email to the user email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # changer en production
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
